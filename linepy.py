29226c7c4096ec86913316786a0448bb17c33b25
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-08-13 21:03:15 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -32,9 +32,10 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.message.text == "nontawat.r":
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event."0080946"))
 
 
 if __name__ == "__main__":
