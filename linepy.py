3ff99c43951113dfbca63472d7d484b0d32a79dd
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-08-13 20:29:32 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -14,18 +14,19 @@ def hello():
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+     # get X-Line-Signature header value
+  #   signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+     # get request body as text
+ #    body = request.get_data(as_text=True)
+  #   app.logger.info("Request body: " + body)
 
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+ #   try:
+ #       handler.handle(body, signature)
+ #   except InvalidSignatureError:
+ #       abort(400)
+    if request.method == 'POST':
 
     return 'OK'
     
