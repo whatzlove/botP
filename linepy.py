64c929f6426233db21e798a884044a754b22489f
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-08-18 21:24:16 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -39,7 +39,7 @@ def handle_message(event):
 #        line_bot_api.reply_message(
  #           event.reply_token,
   #          TextSendMessage(text='USB TEST'))
-        usbc(inputarr[1]))
+        usbcheck.usbc(inputarr[1]))
 	    event.reply_token,
             TextSendMessage(result))	
 	
