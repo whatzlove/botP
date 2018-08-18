7e4b33804f46705d3f658720e119bead4b1bd671
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-08-18 21:50:23 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
-from app.usbcheck import usbc
+from usbcheck import usbc
 
 app = Flask(__name__)
 
