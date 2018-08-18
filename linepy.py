035f426b31b62c892def35a29f9853947622024c
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-08-18 20:25:47 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -32,7 +32,12 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if (event.message.text == "menu") or (event.message.text == "Menu"):
+    textinput = event.message.text.split(' ')
+    if (event.message.text == "usb test")
+	line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(textinput[0])
+    elif (event.message.text == "menu") or (event.message.text == "Menu"):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='Please type \n "install" \n "uninstall"'))
