cd11b6189cbaf423176afc761eac82afac0afa57
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-08-18 21:18:48 +0700

    Update usbcheck.py

--- usbcheck.py
+++ usbcheck.py
@@ -1,19 +1,21 @@
 def usbc(n):
-	stop = 0
-	j = 0
-	file = open('ListUseUSB.txt')
-	for word in file:
-		name = word.split('	')
-		LastArray =  len(name) - 1
+    stop = 0
+    j = 0
+    file = open('ListUseUSB.txt')
+    for word in file:
+	name = word.split('	')
+	LastArray =  len(name) - 1
 	
 	while j <= LastArray:
         if n == name[j]:
-            print('Use USB and CD-ROM')
-            print('j = ',j)
+#            print('Use USB and CD-ROM')
+#            print('j = ',j)
             j = LastArray
-            stop = 1              
+            stop = 1
+	    result='Use USB and CD-ROM'
         else:
             j += 1
-if stop == 0 :
-    print('Block USB and CD-ROM')
+    if stop == 0 :
+        result='Block USB and CD-ROM'
+    return result
 
