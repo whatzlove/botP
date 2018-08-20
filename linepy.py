f50989ee81972a4c96df7982d6d4524602b2687b
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-08-20 13:36:15 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -102,7 +102,7 @@ def handle_message(event):
             TextSendMessage(text='http://www.mediafire.com/file/ohb1h1e9g3jfbm5/McAfee_Endpoint_Security_Uninstall_Manual_for_Win_7.pdf/file')) 
 
     elif (event.message.text == "pic"):
-	image_message = imageSendMessage(
+        image_message = imageSendMessage(
             original_content_url=https://github.com/whatzlove/botP/blob/master/resource/pic/280528.jpg,
             preview_image_url=https://github.com/whatzlove/botP/blob/master/resource/pic/280528.jpg)
         line_bot_api.reply_message(
