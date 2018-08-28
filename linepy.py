68e63397012b668fe89c7ba396bf5f2818e9464c
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-08-28 15:50:21 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -374,7 +374,7 @@ def handle_message(event):
     else:
         if (event.source.group_id == Ca1eed6eefec9ccb0382b34c99b7594a0):
 		
-	else:
+        else:
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text='ไม่มีสิทธิในการใช้งาน USB หรือถ้าไม่ใช่สิ่งที่ค้นหา กรุณาพิมพ์ "menu"'))   
