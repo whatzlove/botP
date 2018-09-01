762060400862ebcdcfb9f8d57b04bc79f1a969b2
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-09-01 18:29:19 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -381,7 +381,8 @@ def handle_message(event):
 	
     elif (inputarr[0].lower() == "de"):
         logfile = open('log.txt')
-        logtxt = event.source.userid + ',' + inputarr[0] + ',' + inputarr[1] + ',' + inputarr[2] + ',' + inputarr[3] + ',' + inputarr[4]
+        profile = line_bot_api.get_profile(event.source.userid)
+        logtxt = profile.user_id + ',' + profile.display_name + ',' + inputarr[0] + ',' + inputarr[1] + ',' + inputarr[2] + ',' + inputarr[3] + ',' + inputarr[4]
         logfile.write(logtxt)
         logfile.close()
         DEmsg = 'Drive Encryption XML File request' + '\n' + 'Computer Name : ' + inputarr[1].upper() + '\n' + 'Send to : ' + inputarr[2] + '\n' + 'Tel. : ' + inputarr[3] + '\n' + 'Remark : ' + inputarr[4]
