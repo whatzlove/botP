2f3ef6f97efe867c37cb7b3f422da0f466ca9e21
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-09-01 18:48:51 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -380,16 +380,16 @@ def handle_message(event):
             TextSendMessage(event.source.group_id)) 
 	
     elif (inputarr[0].lower() == "de"):
-        logfile = open('log.txt','w')
         profile = line_bot_api.get_profile(event.source.user_id)
         logtxt = profile.user_id + ',' + profile.display_name + ',' + inputarr[0] + ',' + inputarr[1] + ',' + inputarr[2] + ',' + inputarr[3] + ',' + inputarr[4] + '\n'
+        logfile = open('log.txt','a')        
         logfile.write(logtxt)
         logfile.close()
         DEmsg = 'Drive Encryption XML File request' + '\n' + 'Computer Name : ' + inputarr[1].upper() + '\n' + 'Send to : ' + inputarr[2] + '\n' + 'Tel. : ' + inputarr[3] + '\n' + 'Remark : ' + inputarr[4]
         msg = DEmsg + '\n' + logtxt
         line_bot_api.push_message(
             'Ua6751e3b8340b1b849c4826ad27ddcdd',
-            TextSendMessage(logtxt)) 
+            TextSendMessage(msg)) 
 	
     else:
         if (event.source.type != 'group'):
