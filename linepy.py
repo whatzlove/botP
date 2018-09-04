ce41ef6c17a94a6f506e16f46b819df1e3ce4b84
whatzlove <42082370+whatzlove@users.noreply.github.com>
2018-09-04 09:37:53 +0700

    Update linepy.py

--- linepy.py
+++ linepy.py
@@ -115,74 +115,74 @@ def handle_message(event):
 	#or (event.message.text == "Install symantec"):
       #  profile = line_bot_api.get_profile(event.source.user_id)
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/3ftXd6c.jpg',
-	    preview_image_url='https://imgur.com/Wg4Shju.jpg')
+            original_content_url='https://i.imgur.com/3ftXd6c.jpg',
+	    preview_image_url='https://i.imgur.com/Wg4Shju.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/c92MnU7.jpg',
-	    preview_image_url='https://imgur.com/54x4ero.jpg')
+            original_content_url='https://i.imgur.com/c92MnU7.jpg',
+	    preview_image_url='https://i.imgur.com/54x4ero.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/w6Zfdgw.jpg',
-	    preview_image_url='https://imgur.com/M0u2HS3.jpg')
+            original_content_url='https://i.imgur.com/w6Zfdgw.jpg',
+	    preview_image_url='https://i.imgur.com/M0u2HS3.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/LdBek5A.jpg',
-	    preview_image_url='https://imgur.com/lyADnLn.jpg')
+            original_content_url='https://i.imgur.com/LdBek5A.jpg',
+	    preview_image_url='https://i.imgur.com/lyADnLn.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/v0xUU9P.jpg',
-	    preview_image_url='https://imgur.com/HsMf8a5.jpg')
+            original_content_url='https://i.imgur.com/v0xUU9P.jpg',
+	    preview_image_url='https://i.imgur.com/HsMf8a5.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/kCFVwdd.jpg',
-	    preview_image_url='https://imgur.com/Q9DzQiZ.jpg')
+            original_content_url='https://i.imgur.com/kCFVwdd.jpg',
+	    preview_image_url='https://i.imgur.com/Q9DzQiZ.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/BJ8zJAC.jpg',
-	    preview_image_url='https://imgur.com/XYKjunB.jpg')
+            original_content_url='https://i.imgur.com/BJ8zJAC.jpg',
+	    preview_image_url='https://i.imgur.com/XYKjunB.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/auT1CVa.jpg',
-	    preview_image_url='https://imgur.com/XYxDUnxB.jpg')
+            original_content_url='https://i.imgur.com/auT1CVa.jpg',
+	    preview_image_url='https://i.imgur.com/XYxDUnxB.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/uEQFBHx.jpg',
-	    preview_image_url='https://imgur.com/KqmQucB.jpg')
+            original_content_url='https://i.imgur.com/uEQFBHx.jpg',
+	    preview_image_url='https://i.imgur.com/KqmQucB.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/WvEQLNV.jpg',
-	    preview_image_url='https://imgur.com/7I3fuiQ.jpg')
+            original_content_url='https://i.imgur.com/WvEQLNV.jpg',
+	    preview_image_url='https://i.imgur.com/7I3fuiQ.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/aXU2brW.jpg',
-	    preview_image_url='https://imgur.com/OWI6viZ.jpg')
+            original_content_url='https://i.imgur.com/aXU2brW.jpg',
+	    preview_image_url='https://i.imgur.com/OWI6viZ.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/dT2CSmQ.jpg',
-	    preview_image_url='https://imgur.com/B3pFR6A.jpg')
+            original_content_url='https://i.imgur.com/dT2CSmQ.jpg',
+	    preview_image_url='https://i.imgur.com/B3pFR6A.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
@@ -190,56 +190,56 @@ def handle_message(event):
     elif (inputc == "uninstall symantec"):
 	#or (event.message.text == "Uninstall symantec"):
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/ONZGYe5.jpg',
-	    preview_image_url='https://imgur.com/MkHtf99.jpg')
+            original_content_url='https://i.imgur.com/ONZGYe5.jpg',
+	    preview_image_url='https://i.imgur.com/MkHtf99.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/i8YlhT0.jpg',
-	    preview_image_url='https://imgur.com/OSzxIWs.jpg')
+            original_content_url='https://i.imgur.com/i8YlhT0.jpg',
+	    preview_image_url='https://i.imgur.com/OSzxIWs.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/RAujq9C.jpg',
-	    preview_image_url='https://imgur.com/RAujq9C.jpg')
+            original_content_url='https://i.imgur.com/RAujq9C.jpg',
+	    preview_image_url='https://i.imgur.com/RAujq9C.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/J22RJWY.jpg',
-	    preview_image_url='https://imgur.com/J22RJWY.jpg')
+            original_content_url='https://i.imgur.com/J22RJWY.jpg',
+	    preview_image_url='https://i.imgur.com/J22RJWY.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/siiLhhT.jpg',
-	    preview_image_url='https://imgur.com/siiLhhT.jpg')
+            original_content_url='https://i.imgur.com/siiLhhT.jpg',
+	    preview_image_url='https://i.imgur.com/siiLhhT.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/jsphX5M.jpg',
-	    preview_image_url='https://imgur.com/jsphX5M.jpg')
+            original_content_url='https://i.imgur.com/jsphX5M.jpg',
+	    preview_image_url='https://i.imgur.com/jsphX5M.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/4kQn5F7.jpg',
-	    preview_image_url='https://imgur.com/4kQn5F7.jpg')
+            original_content_url='https://i.imgur.com/4kQn5F7.jpg',
+	    preview_image_url='https://i.imgur.com/4kQn5F7.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/8IQe6mM.jpg',
-	    preview_image_url='https://imgur.com/8IQe6mM.jpg')
+            original_content_url='https://i.imgur.com/8IQe6mM.jpg',
+	    preview_image_url='https://i.imgur.com/8IQe6mM.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/DmvFCIJ.jpg',
-	    preview_image_url='https://imgur.com/DmvFCIJ.jpg')
+            original_content_url='https://i.imgur.com/DmvFCIJ.jpg',
+	    preview_image_url='https://i.imgur.com/DmvFCIJ.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
@@ -258,74 +258,74 @@ def handle_message(event):
     elif (inputc == "install ens10"): 
 	#or (event.message.text == "Install ens10"):
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/WBr1TGj.jpg',
-	    preview_image_url='https://imgur.com/WBr1TGj.jpg')
+            original_content_url='https://i.imgur.com/WBr1TGj.jpg',
+	    preview_image_url='https://i.imgur.com/WBr1TGj.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/GvgT8LQ.jpg',
-	    preview_image_url='https://imgur.com/GvgT8LQ.jpg')
+            original_content_url='https://i.imgur.com/GvgT8LQ.jpg',
+	    preview_image_url='https://i.imgur.com/GvgT8LQ.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/utkMSES.jpg',
-	    preview_image_url='https://imgur.com/utkMSES.jpg')
+            original_content_url='https://i.imgur.com/utkMSES.jpg',
+	    preview_image_url='https://i.imgur.com/utkMSES.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/P9F3DVr.jpg',
-	    preview_image_url='https://imgur.com/P9F3DVr.jpg')
+            original_content_url='https://i.imgur.com/P9F3DVr.jpg',
+	    preview_image_url='https://i.imgur.com/P9F3DVr.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/UdG5CaW.jpg',
-	    preview_image_url='https://imgur.com/UdG5CaW.jpg')
+            original_content_url='https://i.imgur.com/UdG5CaW.jpg',
+	    preview_image_url='https://i.imgur.com/UdG5CaW.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/fJhqnwj.jpg',
-	    preview_image_url='https://imgur.com/fJhqnwj.jpg')
+            original_content_url='https://i.imgur.com/fJhqnwj.jpg',
+	    preview_image_url='https://i.imgur.com/fJhqnwj.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/uvhvWDx.jpg',
-	    preview_image_url='https://imgur.com/uvhvWDx.jpg')
+            original_content_url='https://i.imgur.com/uvhvWDx.jpg',
+	    preview_image_url='https://i.imgur.com/uvhvWDx.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/wNaFYAM.jpg',
-	    preview_image_url='https://imgur.com/wNaFYAM.jpg')
+            original_content_url='https://i.imgur.com/wNaFYAM.jpg',
+	    preview_image_url='https://i.imgur.com/wNaFYAM.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/wAmqVHa.jpg',
-	    preview_image_url='https://imgur.com/wAmqVHa.jpg')
+            original_content_url='https://i.imgur.com/wAmqVHa.jpg',
+	    preview_image_url='https://i.imgur.com/wAmqVHa.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/kN87Ybn.jpg',
-	    preview_image_url='https://imgur.com/kN87Ybn.jpg')
+            original_content_url='https://i.imgur.com/kN87Ybn.jpg',
+	    preview_image_url='https://i.imgur.com/kN87Ybn.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/U64Y6aa.jpg',
-	    preview_image_url='https://imgur.com/U64Y6aa.jpg')
+            original_content_url='https://i.imgur.com/U64Y6aa.jpg',
+	    preview_image_url='https://i.imgur.com/U64Y6aa.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/6K3Z1r9.jpg',
-	    preview_image_url='https://imgur.com/6K3Z1r9.jpg')
+            original_content_url='https://i.imgur.com/6K3Z1r9.jpg',
+	    preview_image_url='https://i.imgur.com/6K3Z1r9.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
@@ -333,52 +333,52 @@ def handle_message(event):
     elif (inputc == "uninstall ens10"):
 	#or (event.message.text == "Uninstall ens10"):
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/ynsasNL.jpg',
-	    preview_image_url='https://imgur.com/ynsasNL.jpg')
+            original_content_url='https://i.imgur.com/ynsasNL.jpg',
+	    preview_image_url='https://i.imgur.com/ynsasNL.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/9R09HwZ.jpg',
-	    preview_image_url='https://imgur.com/9R09HwZ.jpg')
+            original_content_url='https://i.imgur.com/9R09HwZ.jpg',
+	    preview_image_url='https://i.imgur.com/9R09HwZ.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/OV3qfRx.jpg',
-	    preview_image_url='https://imgur.com/OV3qfRx.jpg')
+            original_content_url='https://i.imgur.com/OV3qfRx.jpg',
+	    preview_image_url='https://i.imgur.com/OV3qfRx.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/Mqxq9Pk.jpg',
-	    preview_image_url='https://imgur.com/Mqxq9Pk.jpg')
+            original_content_url='https://i.imgur.com/Mqxq9Pk.jpg',
+	    preview_image_url='https://i.imgur.com/Mqxq9Pk.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/U2ZBazn.jpg',
-	    preview_image_url='https://imgur.com/U2ZBazn.jpg')
+            original_content_url='https://i.imgur.com/U2ZBazn.jpg',
+	    preview_image_url='https://i.imgur.com/U2ZBazn.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/5UvZvjZ.jpg',
-	    preview_image_url='https://imgur.com/5UvZvjZ.jpg')
+            original_content_url='https://i.imgur.com/5UvZvjZ.jpg',
+	    preview_image_url='https://i.imgur.com/5UvZvjZ.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/jUqDyRc.jpg',
-	    preview_image_url='https://imgur.com/jUqDyRc.jpg')
+            original_content_url='https://i.imgur.com/jUqDyRc.jpg',
+	    preview_image_url='https://i.imgur.com/jUqDyRc.jpg')
         line_bot_api.push_message(
             profile.user_id,image_message
             )
 	
     elif (event.message.text == "ข้าวเที่ยง"):
         image_message = ImageSendMessage(
-            original_content_url='https://imgur.com/4D77SCC.jpg',
-	    preview_image_url='https://imgur.com/hjKN0at.jpg')
+            original_content_url='https://i.imgur.com/4D77SCC.jpg',
+	    preview_image_url='https://i.imgur.com/hjKN0at.jpg')
         line_bot_api.reply_message(
             event.reply_token,image_message
             ) 
